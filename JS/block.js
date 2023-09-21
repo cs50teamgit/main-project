96133deb8775a49cf875de1ac4cd0476a00dd717
Farid_mr <itmirengineering@gmail.com>
2023-09-21 07:33:07 +0330

    added showing negative when hover block

--- JS/block.js
+++ JS/block.js
@@ -1,1 +1,14 @@
-// this is test
+document.addEventListener('DOMContentLoaded', (event) => {
+    const blocks = document.getElementsByClassName('container-block');
+    const hiddenNegatives = document.getElementsByClassName('negative');
+
+    for(let i = 0; i < blocks.length; i++) {
+        blocks[i].addEventListener('mouseover', function handleMouseOver() {
+            hiddenNegatives[i].style.visibility = 'visible';
+        });
+
+        blocks[i].addEventListener('mouseout', function handleMouseOut() {
+            hiddenNegatives[i].style.visibility = 'hidden';
+        });
+    }
+});
